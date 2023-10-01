cc57e7d657378daaec94934ffb42618a512e862c
adibur6 <adibur6@gmail.com>
2023-10-01 20:19:14 +0600

    created a new component subject-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SubjectFormComponent } from './subject-form/subject-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubjectFormComponent
   ],
   imports: [
     BrowserModule
